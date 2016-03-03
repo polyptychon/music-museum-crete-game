271b4b88d08a1aca949b31aebbd9a93c9d85f4aa
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-03-03 11:08:03 +0200

    clean up

--- gulpfile.js
+++ gulpfile.js
@@ -49,6 +49,30 @@ var DEVELOPMENT = 'development',
     watching = false,
     not_in_dependencies_libs = ['jquery', 'bootstrapify'];
 
+var acc = function (t) {
+  t = t.replace(/Ά/g, "Α");
+  t = t.replace(/ά/g, "α");
+  t = t.replace(/Έ/g, "Ε");
+  t = t.replace(/έ/g, "ε");
+  t = t.replace(/Ή/g, "Η");
+  t = t.replace(/ή/g, "η");
+  t = t.replace(/Ί/g, "Ι");
+  t = t.replace(/Ϊ/g, "Ι");
+  t = t.replace(/ί/g, "ι");
+  t = t.replace(/ϊ/g, "ι");
+  t = t.replace(/ΐ/g, "ι");
+  t = t.replace(/Ό/g, "Ο");
+  t = t.replace(/ό/g, "ο");
+  t = t.replace(/Ύ/g, "Υ");
+  t = t.replace(/Ϋ/g, "Υ");
+  t = t.replace(/ύ/g, "υ");
+  t = t.replace(/ϋ/g, "υ");
+  t = t.replace(/ΰ/g, "υ");
+  t = t.replace(/Ώ/g, "Ω");
+  t = t.replace(/ώ/g, "ω");
+  return t;
+};
+
 var env = process.env.NODE_ENV || DEVELOPMENT;
 if (env!==DEVELOPMENT) env = PRODUCTION;
 
@@ -74,6 +98,7 @@ gulp.task('jade', function() {
     "production": env === PRODUCTION,
     "pretty": env === DEVELOPMENT,
     "locals": {
+      'acc': acc,
       'data': JSON.parse(fs.readFileSync("./" + SRC + "/json/el.json", "utf8"))
     }
   };
